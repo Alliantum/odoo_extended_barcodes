7660f42b1134fd8a5b19cac3f4a91db772d56715
David Moreno <david.moreno@alliantum.com>
2021-12-13 14:57:59 +0100

    [ADD] odoo_extended_barcodes initial commit

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "Extended Barcodes",
+    'summary': "",
+    'author': "Alliantum",
+    'website': "https://www.alliantum.com",
+    'license': 'AGPL-3',
+    'version': '12.0.1.0.0',
+    'category': 'Technical',
+    'depends': [
+        'web',
+    ],
+    'application': False,
+    'external_dependencies': {
+        'python': ['pyzint'],  # pip install pyzint
+    },
+}
